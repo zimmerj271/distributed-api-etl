55c9efe8100c483d43e5341879796cce85e6e153
Justin Zimmerman <zimmerj271@pm.me>
2026-01-30 16:50:29 -0700

    Fixes incorrect type hinting in defaultdict

--- src/request_execution/models.py
+++ src/request_execution/models.py
@@ -3,8 +3,8 @@ import hashlib
 import json
 from enum import Enum
 from datetime import datetime
-from dataclasses import dataclass, field 
-from pyspark.sql import Row 
+from dataclasses import dataclass, field
+from pyspark.sql import Row
 from typing import Any, Mapping
 
 
@@ -23,6 +23,7 @@ class RequestMapping:
     This class is intended to be a structural interface for the signature
     of methods in EndpointRuntimeFactory.
     """
+
     param: dict[str, str]
 
 
@@ -31,7 +32,7 @@ class RequestContext:
     """
     A container to hold metadata for a HTTP request.
     • method: HTTP request type - GET, POST, etc
-    • url: Enpoint URL for HTTP request 
+    • url: Enpoint URL for HTTP request
     • auth: A tuple to store the username and password
     • headers: Request headers
     • params: Payload sent as params
@@ -40,9 +41,10 @@ class RequestContext:
     • timeout: Timeout for requests
     • metadata: Metadata associated with the HTTP request
     """
+
     method: RequestType
-    url: str 
-    auth: tuple[str, str] = field(default_factory=tuple)
+    url: str
+    auth: tuple[str, str] = field(default_factory=tuple[str, str])
     headers: dict[str, str] = field(default_factory=dict)
     params: dict[str, Any] | None = None
     param_mapping: dict[str, str] | None = None
@@ -52,10 +54,12 @@ class RequestContext:
     metadata: dict[str, Any] = field(default_factory=dict)
     _row: Row | None = None
 
-    def with_headers(self, context: "RequestContext", new_headers: dict[str, str]) -> "RequestContext":
+    def with_headers(
+        self, context: "RequestContext", new_headers: dict[str, str]
+    ) -> "RequestContext":
         """Return context with headers added"""
         context.headers = context.headers | new_headers
-        return context 
+        return context
 
 
 @dataclass
@@ -63,7 +67,7 @@ class RequestExchange:
     """
     High-level semantic data container of a single HTTP request.
     This container is passed through the Interceptor model as part
-    of the ETL Layer which eventually is used to load to a PySpark 
+    of the ETL Layer which eventually is used to load to a PySpark
     DataFrame.
     • context: original RequestContext, possibly modified by middleware
     • status: HTTP status response code
@@ -75,6 +79,7 @@ class RequestExchange:
     • attempts: how many times the request was attempted (retries)
     • metadata: additional ETL / pipeline metadata
     """
+
     context: RequestContext
     status_code: int | None = None
     headers: dict[str, str] = field(default_factory=dict)
@@ -88,23 +93,23 @@ class RequestExchange:
     def build_row(self, request_id_value: str) -> Row:
         """
         Pass RequestResponse data to a PySpark Row object.
-        Assumes JsonResponseMiddleware has been used to 
+        Assumes JsonResponseMiddleware has been used to
         populate json_body.
-        """        
+        """
         if self.body is None:
             row_hash = None
         else:
             row_hash = hashlib.sha256(self.body).hexdigest()
 
         request_time = datetime.now()
-        
+
         return Row(
             request_id=request_id_value,
             row_hash=row_hash,
             url=self.context.url,
             method=str(self.context.method.name),
             request_headers=json.dumps(self.context.headers, default=str),
-            request_params=json.dumps(self.context.params, default=str), 
+            request_params=json.dumps(self.context.params, default=str),
             request_metadata=json.dumps(self.context.metadata, default=str),
             status_code=int(self.status_code) if self.status_code is not None else None,
             response_headers=json.dumps(self.headers, default=str),
@@ -113,7 +118,7 @@ class RequestExchange:
             error_message=str(self.error_message) if self.error_message else None,
             attempts=int(self.attempts),
             response_metadata=json.dumps(self.metadata, default=str),
-            _request_time=request_time
+            _request_time=request_time,
         )
 
 
@@ -124,8 +129,9 @@ class TransportRequest:
     This data structure allows for the decoupling of the HTTP
     engine from the rest of the pipeline.
     """
-    method: str 
-    url: str 
+
+    method: str
+    url: str
     headers: dict[str, str]
     params: dict[str, Any] | None = None
     json: dict[str, Any] | None = None
@@ -139,6 +145,7 @@ class TransportResponse:
     This data structure allows for the decoupling of the HTTP
     engine from the rest of the pipeline.
     """
+
     status: int | None
     headers: Mapping[str, str] | None
     body: bytes | None
